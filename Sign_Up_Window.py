5c2004b55ffce9928781795a65a6e62ada586df4
sssashhhka <sssashhhka@gmail.com>
2023-05-25 14:54:49 +0800

    [+] Обновлен интерфейс

--- Sign_Up_Window.py
+++ Sign_Up_Window.py
@@ -1,5 +1,25 @@
 import customtkinter as gui
-import smtplib
-from hash_pass import hash_pass
-import db_handler
 
+
+class SignUpWindow(gui.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Sign Up')
+        self.geometry('350x450')
+
+        self.hiLabel_font = gui.CTkFont(family='Segoe UI Variable', size=25, weight='normal')
+        self.midLabel_font = gui.CTkFont(family='Segoe UI Variable', size=20, weight='normal')
+        self.button_font = gui.CTkFont(family='Segoe UI Variable', size=15, weight='normal')
+
+        self.label = gui.CTkLabel(self, text='Sign Up', font=self.hiLabel_font)
+
+        self.label.grid(row=0, column=0, padx=10, pady=10, sticky='new')
+
+        self.columnconfigure(0, weight=1)
+
+
+app = SignUpWindow()
+
+if __name__ == '__main__':
+    app.mainloop()
