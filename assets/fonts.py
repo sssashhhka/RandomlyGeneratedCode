a85929bd8afc2384a56791b998b330ae0b8dd316
sssashhhka <sssashhhka@gmail.com>
2023-06-01 13:46:52 +0800

    [=] Вынос ассетов для удобного пользования

---
+++ assets/fonts.py
@@ -0,0 +1,7 @@
+import customtkinter
+
+root = customtkinter.CTk()
+hi_label_font = customtkinter.CTkFont(family='Segoe UI Variable Display Bold', size=25)
+mid_label_font = customtkinter.CTkFont(family='Segoe UI Variable Display Semibold', size=20, weight='normal')
+button_font = customtkinter.CTkFont(family='Segoe UI Variable Display', size=15, weight='normal')
+root.quit()
