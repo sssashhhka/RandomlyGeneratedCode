a7a4eedb2d64b3f7eceb4d5faa81205ba86c1646
sssashhhka <mihailsnurkov@gmail.com>
2023-07-11 00:08:13 +0800

    Чуть-чуть обновил работу модуля. Добавлена отладка.

--- utility/db_handler.py
+++ utility/db_handler.py
@@ -1,13 +1,37 @@
 import sqlite3
+import time
+import os
+
+version: str = f"OS: [{os.name}] db_handler v.1.0_rc (release candidate)"
+
+
+def log_print(info: str, state: int = 0):
+    if os.path.isfile("logs/log.txt"):
+        f = open("logs/log.txt", "a+")
+    else:
+        f = open("logs/log.txt", "w+")
+    time_is = time.strftime("%X")
+    info_state = None
+    match state:
+        case 0:
+            info_state = "Info"
+        case 1:
+            info_state = "Warning"
+        case 2:
+            info_state = "Error"
+    print(f"[{time_is}][{info_state}] {info}")
+    f.write(f"[{time_is}][{info_state}] {info}\n")
+    f.close()
 
 
 def init():
+    print(version)
     try:
-        db = sqlite3.connect("databases/users.db")
+        db = sqlite3.connect("users/users.db")
         c = db.cursor()
         c.execute("SELECT * FROM USERS")
     except sqlite3.OperationalError:
-        db = sqlite3.connect("databases/users.db")
+        db = sqlite3.connect("users/users.db")
         c = db.cursor()
         c.execute("""CREATE TABLE users (
             username TEXT,
@@ -19,10 +43,12 @@ def init():
         )""")
         db.commit()
         c.close()
+    else:
+        log_print("Database initialized.")
 
 
 def insert(*, username: str, passwd: str, email: str, theme: str = 'Dark', clrtheme: str = 'dark-blue'):
-    db = sqlite3.connect('databases/users.db')
+    db = sqlite3.connect('users/users.db')
     c = db.cursor()
 
     c.execute(f"UPDATE users SET 'isCurrent' = '0' WHERE isCurrent = '1'")
@@ -31,10 +57,11 @@ def insert(*, username: str, passwd: str, email: str, theme: str = 'Dark', clrth
 
     db.commit()
     c.close()
+    log_print("Userdata successfully wrote.")
 
 
 def check(*, return_param: str, **parameters: str):
-    db = sqlite3.connect('databases/users.db')
+    db = sqlite3.connect('users/users.db')
     c = db.cursor()
     if return_param == 'Boolean':
         for parameter, invalue in parameters.items():
@@ -59,8 +86,8 @@ def check(*, return_param: str, **parameters: str):
     c.close()
 
 
-def get(username: str, *args: str):
-    db = sqlite3.connect("databases/users.db")
+def get(username: str, *args: str) -> list:
+    db = sqlite3.connect("users/users.db")
     c = db.cursor()
     parameters_list = []
     for parameter in args:
@@ -70,33 +97,35 @@ def get(username: str, *args: str):
             try:
                 parameters_list.append(value[0])
             except TypeError:
-                print(f'Bad username [{username}]')
+                log_print(f'Bad username [{username}].', 1)
         except sqlite3.OperationalError:
-            print(f'Bad parameter name [{parameter}]')
+            log_print(f'Bad parameter name [{parameter}].', 2)
     c.close()
     return parameters_list
 
 
 def update(*, user: str, **parameters: str):
-    db = sqlite3.connect('databases/users.db')
+    db = sqlite3.connect('users/users.db')
     c = db.cursor()
     c.execute(f"SELECT * FROM users WHERE username = '{user}'")
     for parameter, value in parameters.items():
         try:
             c.execute(f"UPDATE users SET '{parameter}' = '{value}' WHERE username = '{user}'")
+            log_print("Userdata successfully updated.")
         except sqlite3.OperationalError:
-            pass
+            log_print(f"Bad parameters [{parameters.items()}].", 2)
 
     db.commit()
     c.close()
 
 
 def delete(*, user: str):
-    db = sqlite3.connect('databases/users.db')
+    db = sqlite3.connect('users/users.db')
     c = db.cursor()
     c.execute(f"DELETE FROM users WHERE username = '{user}'")
     db.commit()
     c.close()
+    log_print(f"User {user} deleted.")
 
 
 if __name__ != "__main__":
